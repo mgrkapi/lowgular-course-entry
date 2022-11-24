4373362c1250ef1734523fa7f6461a5c7df030b2
mgrkapi <mgrkapi@wp.pl>
2022-11-24 13:56:08 +0100

    feat:component with images, names and styles added

---
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {EmployeeModel} from "../../model/employee.model";
+
+@Component({
+  selector: 'app-employees-faces',
+  templateUrl: './employees-faces.component.html',
+  styleUrls: ['./employee-faces.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeesFacesComponent {
+  constructor(private _httpClient: HttpClient) {}
+    data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
+}
